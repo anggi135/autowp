0616a42fea9d39f2d3a9bdec1463451506db231d
up shell <40010260+anggi135@users.noreply.github.com>
2023-04-16 17:01:30 +0700

    Add files via upload

---
+++ auto.php
@@ -0,0 +1,24 @@
+<?php
+
+// memanggil file wp-load.php untuk mengakses fungsi WordPress
+require_once( dirname( __FILE__ ) . 'rss_xml.php' );
+
+// mengatur interval waktu untuk eksekusi kode (dalam detik)
+$interval = 3600;
+
+// menentukan waktu awal
+$start_time = time();
+
+// menjalankan kode setiap interval waktu
+while (true) {
+  // memanggil kode untuk memproses feed RSS dan memasukkan konten ke WordPress
+  include 'process_rss.php';
+
+  // menghitung waktu yang diperlukan untuk menjalankan kode
+  $time_elapsed = time() - $start_time;
+
+  // menunggu sisa waktu interval sebelum menjalankan kode lagi
+  sleep($interval - $time_elapsed);
+  $start_time = time();
+}
+?>
---
+++ rss_xml.php
@@ -0,0 +1,2 @@
+$url = 'https://contoh.com/feed/';
+$xml = file_get_contents($url);
